8e2788c764b4f120a1a2bc53268b9f98877bb761
FIC <FIC@info.local>
2019-12-17 10:49:24 -0300

    c#

---
+++ MousePoo/Models/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MousePoo.Models {
+  public class Mousemouse1 {
+    public double Tamanho { get; set; }
+
+    public string Marca { get; set; }
+
+    public string Botoes { get; set; }
+
+    public string Material { get; set; }
+
+    public string Modelo { get; set; }
+  }
+}
---
+++ MousePoo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MousePoo.Models;
+
+
+namespace MousePoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mousemouse1 rato1 = new Mousemouse1();
+
+            Console.Clear();
+
+            System.Console.WriteLine("Insira o Tamanho");
+            rato1.Tamanho = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Insira a Marca");
+            rato1.Marca = Console.ReadLine();
+
+            System.Console.WriteLine("Insira o Botoes");
+            rato1.Botoes = Console.ReadLine();
+
+            System.Console.WriteLine("Insira o Material");
+            rato1.Material = Console.ReadLine();
+
+            System.Console.WriteLine("Insira o Modelo");
+            rato1.Modelo = Console.ReadLine();
+
+
+            Console.Clear();
+
+            System.Console.WriteLine($@"
+            Tamanho:{rato1.Tamanho} 
+            Marca:{rato1.Marca}
+            Botões:{rato1.Botoes}
+            Material:{rato1.Material}
+            Modelo:{rato1.Modelo}
+            ");
+
+
+
+
+        }
+    }
+}
